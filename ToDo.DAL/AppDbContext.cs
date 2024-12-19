bf3d2ea15e8420260b56ce64583d29f28c0f40f6
Geb <korotkiygs@gmail.com>
2024-12-19 23:16:29 +0300

    Определяем подключение к БД

--- ToDo.DAL/AppDbContext.cs
+++ ToDo.DAL/AppDbContext.cs
@@ -1,8 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using ToDo.Domain.Entity;
 
 namespace ToDo.DAL;
 
 public class AppDbContext: DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+    public DbSet<TaskEntity> Tasks { get; set; }
 }
